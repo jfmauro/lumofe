b7928c2e5875fae936a487a3d115aa4e29917afb
jeff.mauro <jeff.mauro@gmail.com>
2026-03-18 22:06:46 +0100

    new ui

--- app/src/main/java/be/tivano/lumo/ui/MainActivity.kt
+++ app/src/main/java/be/tivano/lumo/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package be.tivano.lumo.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import be.tivano.lumo.R
 import be.tivano.lumo.data.RetrofitClient
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         RetrofitClient.initialize(this)
+        setContentView(R.layout.activity_main)
         // TODO: inflate main dashboard layout (US-0.1.2+)
     }
 }
